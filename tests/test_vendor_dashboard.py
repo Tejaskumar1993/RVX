ec059143163cd561b94b0d0d3ec0a42756c2280a
shreyabeladiya <shreyabeladiya@ravaremtech.com>
2025-01-21 15:44:05 +0530

    added test-cases for the vendor dashboard page

---
+++ tests/test_vendor_dashboard.py
@@ -0,0 +1,49 @@
+from pytest_playwright.pytest_playwright import page
+from qase.pytest import qase
+from conftest import dictionary_parametrize
+from data.vendor_dashboard import VendorDashboardParams
+from conftest import vendor_dashboard_page
+
+
+@dictionary_parametrize(
+    {
+        "Test_1": VendorDashboardParams.Test_1,
+    }
+)
+@qase.title("Verify vendor Dashboard elements")
+# @qase.id(2)
+@qase.fields(
+    ("severity", "major"),
+    ("priority", "high"),
+    (
+            "description",
+            "Verify all fields of vendor Dashboard",
+    ),
+)
+def test_verify_vendor_dashboard_page_elements(
+        environment_to_run,
+        ontrack_username,
+        ontrack_password,
+        ontrack_login_page,
+        vendor_dashboard_page,
+        role_to_change,
+        sold_products_this_week_table_headers,
+        most_sold_products_by_publish_table_headers,
+):
+    """
+    Regression test for items list page fields verification
+    """
+    # 1. Navigate to ontrack login page and verify all element of login page
+    ontrack_login_page.open_and_verify_ontrack_login_page(
+        environment_to_run, ontrack_username, ontrack_password
+    )
+    # 2. Change user role
+    vendor_dashboard_page.click_on_dropdown_and_change_user_role(
+        role_to_change=role_to_change
+    )
+    # 3. Click and Verify items list tab
+    vendor_dashboard_page.verify_dashboard_tab()
+    # 4. Verify dashboard page functionality and elements
+    vendor_dashboard_page.verify_dashboard_page_elements(
+        sold_products_this_week_table_headers=sold_products_this_week_table_headers,
+        most_sold_products_by_publish_table_headers=most_sold_products_by_publish_table_headers)
